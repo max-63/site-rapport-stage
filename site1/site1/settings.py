ca838046c6f68a8ad28038ae2d2b216eb13a4a0d
max-63 <adriencourault@outlook.fr>
2025-03-30 13:06:31 +0200

    commit

--- site1/site1/settings.py
+++ site1/site1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nodw0m@_gpm&f8@f(gzyg&1q5f6*r*6**j+z+3x-=5)e=-3_o)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['site-rapport-stage.onrender.com', 'localhost', '127.0.0.1']
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #attaching Whitenoise middleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,7 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+# Emplacement où seront stockés les fichiers statiques après la commande collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# URL utilisée pour accéder aux fichiers statiques
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
